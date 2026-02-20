e64e395b7519d560844ad65fd05f680581eda37c
hughnhugh <hughnguyen@outlook.com>
2026-02-20 16:46:24 +1100

    first commit

---
+++ tests/MantelLogParser.Tests/Parsing/LogParserTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MantelLogParser.Tests.Parsing
+{
+    internal class LogParserTests
+    {
+    }
+}
